ca01c5ea8345ea1b805d3fb225d2006d2abf66e2
Zoe Monet Williams <mwilliamszoe@gmail.com>
2020-02-11 15:04:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
+# ### 3. Define a `Person` in `lib/person.rb`
+
+# Open `lib/person.rb` and add a class definition for a `Person` class.
+
+# ### 4. Make 2 people in `lib/person.rb`
+
+# Under your `Person` class definition, create two people in local variables, `adele_goldberg` and `alan_kay`
+
 class Person
 end
 
