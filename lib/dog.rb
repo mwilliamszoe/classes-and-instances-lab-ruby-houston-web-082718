d65b1ce210f2ce2c395409f65bf4b519dd162ec8
Zoe Monet Williams <mwilliamszoe@gmail.com>
2020-02-11 15:03:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
-class Dog
-end
+### 1. Define `Dog` in `lib/dog.rb`
 
-fido = Dog.new
-snoopy = Dog.new
-lassie = Dog.new
+Open `lib/dog.rb` and add a class definition for a `Dog` class.
+
+### 2. Make 3 dogs in `lib/dog.rb`
+
+Under your `Dog` class definition, create three dogs in local variables, `fido`, `snoopy`, and `lassie`.
